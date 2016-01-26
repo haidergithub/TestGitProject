c3da3f76746dccdd75087ab963e9458ac9c2c351
haidergithub <haider78@gmail.com>
2016-01-26 16:56:17 +0500

    Creae new Project

---
+++ TestGitProject/src/com/test/TestClass.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		String ar[] = {"","","",""};
+		args = ar;
+		for (int i = 0; i < args.length; i++) {
+			
+			System.out.println("ddd");
+		}
+	}
+}
